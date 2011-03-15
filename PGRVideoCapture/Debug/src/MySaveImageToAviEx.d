198f4c4b08801cad02eb45852b8b1dd9ec26df9c
Aras Balali Moghaddam <arasbm@gmail.com>
2011-03-15 04:43:58 -0700

    A little program to capture video from pgr cameras

---
+++ PGRVideoCapture/Debug/src/MySaveImageToAviEx.d
@@ -0,0 +1,37 @@
+src/MySaveImageToAviEx.d src/MySaveImageToAviEx.o: \
+ ../src/MySaveImageToAviEx.cpp /usr/include/flycapture/FlyCapture2.h \
+ /usr/include/flycapture/FlyCapture2Platform.h \
+ /usr/include/flycapture/FlyCapture2Defs.h \
+ /usr/include/flycapture/Error.h /usr/include/flycapture/BusManager.h \
+ /usr/include/flycapture/Camera.h /usr/include/flycapture/CameraBase.h \
+ /usr/include/flycapture/GigECamera.h /usr/include/flycapture/Image.h \
+ /usr/include/flycapture/Utilities.h \
+ /usr/include/flycapture/AVIRecorder.h \
+ /usr/include/flycapture/TopologyNode.h \
+ /usr/include/flycapture/ImageStatistics.h
+
+/usr/include/flycapture/FlyCapture2.h:
+
+/usr/include/flycapture/FlyCapture2Platform.h:
+
+/usr/include/flycapture/FlyCapture2Defs.h:
+
+/usr/include/flycapture/Error.h:
+
+/usr/include/flycapture/BusManager.h:
+
+/usr/include/flycapture/Camera.h:
+
+/usr/include/flycapture/CameraBase.h:
+
+/usr/include/flycapture/GigECamera.h:
+
+/usr/include/flycapture/Image.h:
+
+/usr/include/flycapture/Utilities.h:
+
+/usr/include/flycapture/AVIRecorder.h:
+
+/usr/include/flycapture/TopologyNode.h:
+
+/usr/include/flycapture/ImageStatistics.h:
